148a8eceacf6a18f1bc4ba012e251b2c1698bc0c
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-08 14:54:47 +0000

    Making languages
    git-svn-id: https://plugins.svn.wordpress.org/register-ip-multisite/trunk@309252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -14,6 +14,7 @@ Spam is one thing, but trolls and sock puppets are another.  Sometimes people ju
 When a user registers, their IP is logged in the `wp_usermeta` under the signup_ip key.  Log into your WP install as an Admin and you can look at their profile or the users table to see what it is. For security purposes their IP is not displayed to them when they see their profile.
 
 **Misc**
+
 * [Donate](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5227973)
 * [Plugin Site](http://code.ipstenu.org/register-ip-ms/)
 
--- register-ip.php
+++ register-ip.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Register IP - MultiSite
-Version: 0.1
+Version: 0.2
 Description: Logs the IP of the user when they register a new account.
 Author: Mika Epstein, Johnny White
 Author URI: http://ipstenu.org
@@ -24,6 +24,9 @@ Taken over in 2010 by Mika Epstein under GPL provisons
 
 */
 
+/* Internationlization */
+load_plugin_textdomain('ripm_ip', false, dirname(plugin_basename(__FILE__)) . '/language');
+
 /* Version Check - This only works on 3.x branch! */
 global $wp_version;
 
@@ -36,7 +39,7 @@ if(version_compare($wp_version, "3.0", "<"))
 
 function log_ip($user_id){
         $ip = $_SERVER['REMOTE_ADDR']; //Get the IP of the person registering
-        update_user_meta($user_id, 'signup_ip', $ip); //Add user metadata to the usermeta table
+        update_user_meta($user_id, 'ripm_ip', $ip); //Add user metadata to the usermeta table
 }
 
 // Hook into when the user is registered.
@@ -49,29 +52,29 @@ add_action('edit_user_profile', 'show_ip_on_profile');
 function show_ip_on_profile() {
         $user_id = $_GET['user_id'];
 ?>
-        <h3>Signup IP Address</h3>
+        <h3><?php _e('Signup IP Address',ripm_ip); ?></h3>
         <p style="text-indent:15px;"><?php
-        $ip_address = get_user_meta($user_id, 'signup_ip', true);
+        $ip_address = get_user_meta($user_id, 'ripm_ip', true);
         echo $ip_address;
         ?></p>
 <?php
 }
 
 // Add in a column header
-function signup_ip($column_headers) {
-    $column_headers['signup_ip'] = 'IP Address';
+function ripm_ip($column_headers) {
+    $column_headers['ripm_ip'] = '.__(IP Address, 'ripm_ip').';
     return $column_headers;
 }
 
 // Formatting output for Single Site. Only called if Multisite is disabled.
 function ripms_single_columns($value, $column_name, $user_id) {
-        if ( $column_name == 'signup_ip' ) {
-                $ip = get_user_meta($user_id, 'signup_ip', true);
+        if ( $column_name == 'ripm_ip' ) {
+                $ip = get_user_meta($user_id, 'ripm_ip', true);
                 if ($ip != ""){
-                        $ret = '<em>'.__($ip, 'signup_ip').'</em>';
+                        $ret = '<em>'.__($ip, 'ripm_ip').'</em>';
                         return $ret;
                 } else {
-                        $ret = '<em>'.__('None Recorded', 'signup_ip').'</em>';
+                        $ret = '<em>'.__('None Recorded', 'ripm_ip').'</em>';
                         return $ret;
                 }
         }
@@ -84,13 +87,13 @@ function ripms_single_columns($value, $column_name, $user_id) {
 // Change manage_users_custom_column to ms_manage_users_custom_column when the patch is implimented
 
 function ripms_multi_columns($column_name, $user_id ) {
-        if ( $column_name == 'signup_ip' ) {
-                $ip = get_user_meta($user_id, 'signup_ip', true);
+        if ( $column_name == 'ripm_ip' ) {
+                $ip = get_user_meta($user_id, 'ripm_ip', true);
                 if ($ip != ""){
-                        $ret = '<em>'.__($ip, 'signup_ip').'</em>';
+                        $ret = '<em>'.__($ip, 'ripm_ip').'</em>';
                         echo $ret;
                 } else {
-                        $ret = '<em>'.__('None recorded', 'signup_ip').'</em>';
+                        $ret = '<em>'.__('None recorded', 'ripm_ip').'</em>';
                         echo $ret;
                 }
         }
@@ -98,11 +101,11 @@ function ripms_multi_columns($column_name, $user_id ) {
 
 if ( is_multisite() ) {
 	// Adding in filters and actions for Multi Site installs
-	add_filter('wpmu_users_columns', 'signup_ip');
+	add_filter('wpmu_users_columns', 'ripm_ip');
 	add_action('manage_users_custom_column',  'ripms_multi_columns', 10, 2);
 } else {
 	// Adding in filters and actions for Single Site installs
-	add_filter('manage_users_columns', 'signup_ip');
+	add_filter('manage_users_columns', 'ripm_ip');
 	add_action('manage_users_custom_column',  'ripms_single_columns', 10, 3);
 }
 
